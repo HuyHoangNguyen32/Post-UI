c84a2322fd8b900f7e879aa5293b60865d69b46b
HuyHoangNguyen32 <92217902+HuyHoangNguyen32@users.noreply.github.com>
2023-05-17 14:51:59 +0900

    ✨ Redirect to post detail page on click

--- js/utils/post.js
+++ js/utils/post.js
@@ -46,6 +46,13 @@ export function createPostElement(post) {
   }
 
   // attach events
+  // go to post detail when click on div.post-item
+  const divElement = liElement.firstElementChild
+  if (divElement) {
+    divElement.addEventListener('click', (e) => {
+      window.location.assign(`/post-detail.html?id=${post.id}`)
+    })
+  }
 
   return liElement;
 }
