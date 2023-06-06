f645da4e1b4e1e56ac9411fa9155cdd28b1d9350
HuyHoangNguyen32 <92217902+HuyHoangNguyen32@users.noreply.github.com>
2023-06-06 14:35:42 +0900

    ✨ show / hide control to change image

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -151,6 +151,20 @@ function initRandomImage(form) {
   });
 }
 
+function renderImageSourceControl(form, selectedValue) {
+  const controlList = form.querySelectorAll('[data-id="imageSource"');
+  controlList.forEach(control => {
+    control.hidden = control.dataset.imageSource !== selectedValue;
+  })
+}
+
+function initRadioImageSource(form) {
+  const radioList = form.querySelectorAll('[name="imageSource"]');
+  radioList.forEach((radio) => {
+    radio.addEventListener('change', (e) => renderImageSourceControl(form, e.target.value));
+  });
+}
+
 export function initPostForm({ formId, defaultValues, onSubmit }) {
   const form = document.getElementById(formId);
   if (!form) return;
@@ -160,6 +174,7 @@ export function initPostForm({ formId, defaultValues, onSubmit }) {
 
   // init events
   initRandomImage(form);
+  initRadioImageSource(form);
 
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
